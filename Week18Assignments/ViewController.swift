71bfa82a481f67ee0dd32152056e3f72f095ea0f
Doy Kim <kimdee.dev@gmail.com>
2022-11-02 12:56:50 +0900

    Initial Commit

---
+++ Week18Assignments/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Week18Assignments
+//
+//  Created by Doy Kim on 2022/11/02.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
