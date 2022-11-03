8923af96963176262defad0e24607f7792331986
Doy Kim <kimdee.dev@gmail.com>
2022-11-04 01:09:08 +0900

    [Add] 로그인 뷰모델, 모델 선언

---
+++ Week18Assignments/Model/Login.swift
@@ -0,0 +1,12 @@
+//
+//  Login.swift
+//  Week18Assignments
+//
+//  Created by Doy Kim on 2022/11/03.
+//
+
+import Foundation
+
+struct Login: Codable {
+    let token: String
+}
---
+++ Week18Assignments/ViewModel/LoginViewModel.swift
@@ -0,0 +1,28 @@
+//
+//  LoginViewModel.swift
+//  Week18Assignments
+//
+//  Created by Doy Kim on 2022/11/03.
+//
+
+import Foundation
+import RxSwift
+
+class LoginViewModel {
+    
+    let loginResponse = PublishSubject<Bool>()
+    
+    func postLogin(email: String, password: String) {
+        let api = SeSACAPI.login(email: email, password: password)
+        
+        SeSACNetwork.shared.reqeustSeSAC(ofType: Login.self, url: api.url, method: .post, parameters: api.parameters, headers: api.headers) { [weak self] response in
+            switch response {
+            case .success(let success):
+                UserDefaults.token = success.token
+                self?.loginResponse.onNext(true)
+            case .failure(let failure):
+                self?.loginResponse.onError(failure)
+            }
+        }
+    }
+}
