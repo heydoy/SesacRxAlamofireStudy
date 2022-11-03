1e85a1993c66d20558cb0271e988c647b5288ef0
Doy Kim <kimdee.dev@gmail.com>
2022-11-04 01:18:51 +0900

    [Add] 로그인 VC에 회원가입, 로그인버튼 rx

--- Week18Assignments/Presentation/Login/LoginViewController.swift
+++ Week18Assignments/Presentation/Login/LoginViewController.swift
@@ -6,24 +6,50 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
 
-class LoginViewController: UIViewController {
-
+class LoginViewController: BaseViewController {
+    
+    let mainView = LoginView()
+    let disposeBag = DisposeBag()
+    let viewModel = LoginViewModel()
+    
+    override func loadView() {
+        view = mainView
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        mainView.backgroundColor = .orange
+        
+        bind()
+    }
+    
+    func bind () {
+        
+        mainView.loginButton
+            .rx.tap
+            .withUnretained(self)
+            .bind { (vc, _) in
+                vc.viewModel.postLogin(email: "hue@hue.com", password: "hue12345")
+                print(UserDefaults.token)
+                
+                
+            }
+            .disposed(by: disposeBag)
+        
+        mainView.goSignupButton
+            .rx.tap
+            .bind { [weak self] _ in
+                let vc = SignupViewController()
+                vc.modalPresentationStyle = .fullScreen
+                
+                self?.present(vc, animated: true, completion: nil)
+            }
+            .disposed(by: disposeBag)
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
