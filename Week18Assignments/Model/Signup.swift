c7885139fc39b132fa9cd3c1b92bc6901b85dae7
Doy Kim <kimdee.dev@gmail.com>
2022-11-03 20:49:19 +0900

    [Add] 회원가입 모델 추가

---
+++ Week18Assignments/Model/Signup.swift
@@ -0,0 +1,14 @@
+//
+//  Signup.swift
+//  Week18Assignments
+//
+//  Created by Doy Kim on 2022/11/03.
+//
+
+import Foundation
+
+struct Signup {
+    var nickname: String
+    var email: String
+    var password: String
+}
