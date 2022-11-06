d83ccb65463f57894bb63265e510e05793874d8d
Doy Kim <kimdee.dev@gmail.com>
2022-11-06 23:32:13 +0900

    [Refactor] 닉네임, 이메일, 비밀번호 유효성 체크하는 메서드를 하나로 합치고, 정규식을 열거형으로 바꿔서 매개변수로 받도록 변경

--- Week18Assignments/Global/Extensions/String+Extension.swift
+++ Week18Assignments/Global/Extensions/String+Extension.swift
@@ -7,30 +7,20 @@
 
 import Foundation
 
+enum StringPolicy: String {
+    case nicknameRegex = #"^[가-힣a-zA-Z0-9]{2,10}$"# // 한글, 알파벳, 숫자 포함 2~10글자
+    case emailRegex = #"^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])$"# // 이메일 유효한지 정규식으로 체크
+    case passwordRegex = #"^(?=.*\d)(?=.*[a-z])(?=.*[a-zA-Z]).{8,}$"# // 8글자 이상, 영어소문자, 숫자 반드시 포함
+}
+
 extension String {
     /// 닉네임 유효한지 정규식으로 체크
     /// 한글, 알파벳, 숫자 포함 2~10글자
-    func isValidNickname() -> Bool {
-        let nicknameRegex = #"^[가-힣a-zA-Z0-9]{2,10}$"#
-        let nicknameTest = NSPredicate(format: "SELF MATCHES %@", nicknameRegex)
-        return nicknameTest.evaluate(with: self)
+    func isValidString(_ policy: StringPolicy) -> Bool {
+        let regex = policy.rawValue
+        let test = NSPredicate(format: "SELF MATCHES %@", regex)
+        return test.evaluate(with: self)
     }
     
-    /// 이메일 유효한지 정규식으로 체크
-    func isValidEmail() -> Bool {
-        let emailRegex = #"^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])$"#
-        let emailTest = NSPredicate(format: "SELF MATCHES %@", emailRegex)
-        
-        return emailTest.evaluate(with: self)
-    }
-    
-    /// 비밀번호 유효한지 정규식으로 체크
-    /// 8글자 이상, 영어소문자, 숫자 반드시 포함
-    func isValidPassword() -> Bool {
-        let passwordRegex = #"^(?=.*\d)(?=.*[a-z])(?=.*[a-zA-Z]).{8,}$"#
-        let passwordTest = NSPredicate(format: "SELF MATCHES %@", passwordRegex)
-        
-        return passwordTest.evaluate(with: self)
-    }
 }
 
